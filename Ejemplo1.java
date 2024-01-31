c2b6068f88d3828e46fb319902e259c9ac2e9344
MartaIniesta <158085005+MartaIniesta@users.noreply.github.com>
2024-01-31 19:21:27 +0100

    Update Ejemplo1.java

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -3,12 +3,9 @@ public class Ejemplo1{
         System.out.println("Hola mundo");
         String nombre = "Marta";
         System.out.println("Mi nombre es --> " + nombre);
-<<<<<<< HEAD
         System.out.pritnln("La suma de 2 + 3 es " + (2+3));
-=======
         for(int i = 1; i <= 10; i++){
             System.out.println("Vuelta " + i);
         }
->>>>>>> ED47
     }
 }
